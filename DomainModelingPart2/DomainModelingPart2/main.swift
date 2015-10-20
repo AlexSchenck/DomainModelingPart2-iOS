4c6ffdd1c9806f431d0e0cf11e34febe3c5a2c96
Alex Schenck <AlexNSchenck@gmail.com>
2015-10-19 19:36:38 -0700

    Initial commit

---
+++ DomainModelingPart2/DomainModelingPart2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DomainModelingPart2
+//
+//  Created by iGuest on 10/19/15.
+//  Copyright (c) 2015 Alex Schenck. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
